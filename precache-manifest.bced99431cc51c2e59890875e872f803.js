bee0993a13f0a237959e03e561f5e85620651fc2
Ken Ting <kaleungting@gmail.com>
2020-06-16 20:44:38 -0400

    Updates

---
+++ precache-manifest.bced99431cc51c2e59890875e872f803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379a1377fb275f923c1ae55baddcd52b",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "0ef2ce2b744a9c678f11",
+    "url": "/monsters-inc/static/css/main.42d51d1d.chunk.css"
+  },
+  {
+    "revision": "5ceb71a0b3552c2613c8",
+    "url": "/monsters-inc/static/js/2.95da3331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-inc/static/js/2.95da3331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef2ce2b744a9c678f11",
+    "url": "/monsters-inc/static/js/main.7f9e4201.chunk.js"
+  },
+  {
+    "revision": "1310fce59a7c2dc6a64a",
+    "url": "/monsters-inc/static/js/runtime-main.35f7105d.js"
+  }
+]);
